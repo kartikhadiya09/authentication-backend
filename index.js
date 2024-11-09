4de97810547d84edfabf4967a4e5e88e7265dd22
unknown <ajaymakvana05>
2024-11-09 23:20:33 +0530

    set up files & package

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
-const express=require('express')
-const dotenv=require('dotenv')
+const express = require('express')
+require("dotenv").config();
 
 
-const Port = process.env.PORT
+// Initialize the Express app
+const app = express();
 
+// Get the port from the environment variable (use)
+const PORT = process.env.PORT || 5000;
+
+// Middleware to parse JSON
+app.use(express.json());
+
+// Example route
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
+
+app.get('/login', (req, res) => {
+    res.send('Login');
+});
+
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
 
 
